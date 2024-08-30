26135e1f447702ee49413120212850ef84e9c001
KTajerbashi <kamrantajerbashi@gmail.com>
2024-08-30 17:30:30 +0330

    Callback Fun

--- app.ts
+++ app.ts
@@ -1,14 +1,7 @@
-type Combine = string | number | boolean;
-function Logger(value: Combine): void {
-  console.log("Result ", value);
-}
-
-function add(input1: number, input2: number): number {
-  Logger(input1 + input2);
-  return input1 + input2;
-}
-let funCombine: (input1: number, input2: number) => number;
-
-funCombine = add;
-
-funCombine(1, 2);
+const CalbackFunc = (input1: number, input2: number, cb) => {
+  const result = input1 + input2;
+  cb(result);
+};
+CalbackFunc(10, 22, (item) => {
+  console.log(item);
+});
