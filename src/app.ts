07a8325230de4895a330f1243b645c42e8daebc6
KTajerbashi <kamrantajerbashi@gmail.com>
2024-08-30 21:28:08 +0330

    Add Class Decorator Factory And Render Element

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 //  Decorator
-function Logger(target: Function) {
-  console.log("Logger ...");
+function Logger(logString: string) {
+  return (target: Function) => {
+    console.log(logString);
+    console.log(target);
+  };
 }
 
-@Logger
+function addToDom(content: string, id: string) {
+  return function (_: Function) {
+    const element = document.getElementById(id);
+    console.log("Render Template", element?.id);
+    if (element) {
+      element.innerHTML = content;
+      let counter = 0;
+      element.addEventListener("click", () => {
+        console.log(`Counter ${++counter}`);
+      });
+    }
+  };
+}
+
+@Logger("Logging Product ...")
+@addToDom('<a class="btn btn-primary">First</a>', "app1")
+@addToDom('<a class="btn btn-dark">Second</a>', "app2")
+@addToDom('<a class="btn btn-danger">Thirth</a>', "app3")
 class Product {
   title = "Book";
 
